edede28d55c7f5c823ba4dfb5e98b30aa1a526a5
Gianna Torpey <gianna.torpey@gmail.com>
2025-09-05 17:13:07 -0400

    [React Router] Replaced local footer with shared component

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import React, { useState } from "react";
 
@@ -161,52 +162,7 @@ function Faq() {
       </section>
 
       {/* Footer */}
-      <footer className="footer">
-        <div className="footer-content">
-          <div className="footer-section">
-            <img
-              src="./images/Asset 1.png"
-              alt="MIT AGA Logo"
-              className="footer-aga-logo"
-            />
-            <h3>MIT African Graduate Association</h3>
-            <p>
-              Where African scholarship meets innovation and global influence.
-            </p>
-            <div className="social-links">
-              <a href="#">
-                <i className="fab fa-facebook"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="https://www.linkedin.com/company/mit-african-graduate-association/?viewAsMember=true">
-                <i className="fab fa-linkedin"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram"></i>
-              </a>
-            </div>
-          </div>
-          <div className="footer-section">
-            <img
-              src="./images/mit_logo_std_rgb_white.png"
-              alt="MIT Logo"
-              className="footer-mit-logo"
-            />
-            <h3>Massachusetts Institute of Technology</h3>
-            <p>77 Massachusetts Avenue, Cambridge, MA, USA</p>
-            <a href="#" className="footer-accessibility">
-              Accessibility
-            </a>
-          </div>
-        </div>
-        <div className="footer-bottom">
-          <p>
-            &copy; 2025 MIT African Graduate Association. All rights reserved.
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }

c3b3a68b3e2cc80331c93f9b3ea98ffe94925fa6
Gianna Torpey <gianna.torpey@gmail.com>
2025-09-08 21:27:20 -0400

    Added HashRouter for GithubPages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = document.getElementById("root");
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
   );
 }
